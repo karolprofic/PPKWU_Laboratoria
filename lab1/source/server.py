c65c3a0bb4382c723c0dd9ef324ad6fc54baffc0
Karol Profic <karolprofic@protonmail.com>
2022-10-27 12:59:07 +0200

    Zamiana lab1 z lab2 pisałem w złym katalogu

--- lab1/source/server.py
+++ lab1/source/server.py
@@ -8,27 +8,19 @@ import time
 
 class web_server(http.server.SimpleHTTPRequestHandler):
     
-    def prepare_headers(self):
+    def do_GET(self):
+
+        print(self.path)
+        
+        if self.path == '/':
             self.protocol_version = 'HTTP/1.1'
             self.send_response(200)
             self.send_header("Content-type", "text/html; charset=UTF-8")
-            self.end_headers()     
-
-    def do_GET(self):
-
-        print("Request url: " + self.path)
-        
-        if self.path == '/':
-            self.prepare_headers()
+            self.end_headers()            
             self.wfile.write(b"Hello World!\n")
-        elif self.path.startswith('/cmd=time'):
-            self.prepare_headers()
             named_tuple = time.localtime()
             time_string = time.strftime("%H:%M:%S", named_tuple)
             self.wfile.write(time_string.encode("utf-8"))
-        elif self.path.startswith('/cmd=rev'):
-            self.prepare_headers()
-            self.wfile.write(b"Hello World!\n")
         else:
             super().do_GET()
     
--- lab2/source/server.py
+++ lab2/source/server.py
@@ -4,31 +4,43 @@ import socketserver
 import os
 import time
 
-#print('source code for "http.server":', http.server.__file__)
+
+# print('source code for "http.server":', http.server.__file__)
 
 class web_server(http.server.SimpleHTTPRequestHandler):
-    
+
+    def prepare_headers(self):
+        self.protocol_version = 'HTTP/1.1'
+        self.send_response(200)
+        self.send_header("Content-type", "text/html; charset=UTF-8")
+        self.end_headers()
+
     def do_GET(self):
 
-        print(self.path)
-        
+        print("Request url: " + self.path)
+
         if self.path == '/':
-            self.protocol_version = 'HTTP/1.1'
-            self.send_response(200)
-            self.send_header("Content-type", "text/html; charset=UTF-8")
-            self.end_headers()            
-            self.wfile.write(b"Hello World!\n")
+            self.prepare_headers()
+            self.wfile.write(b"Hello World!")
+        elif self.path.startswith('/?cmd=time'):
+            self.prepare_headers()
             named_tuple = time.localtime()
             time_string = time.strftime("%H:%M:%S", named_tuple)
             self.wfile.write(time_string.encode("utf-8"))
+        elif self.path.startswith('/?cmd=rev'):
+            self.prepare_headers()
+            parameters = self.path.split("&str=")
+            print(parameters)
+            self.wfile.write(b"Hello World!")
         else:
             super().do_GET()
-    
+
+
 # --- main ---
 
 PORT = 4080
 
 print(f'Starting: http://localhost:{PORT}')
 
-tcp_server = socketserver.TCPServer(("",PORT), web_server)
+tcp_server = socketserver.TCPServer(("", PORT), web_server)
 tcp_server.serve_forever()
