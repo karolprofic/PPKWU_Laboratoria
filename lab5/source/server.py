6f0665fdcd80b40e7179ec11aa3f187baa36737a
Karol Profic <karolprofic@protonmail.com>
2022-12-08 12:56:09 +0100

    dODANIE IF

--- lab5/source/server.py
+++ lab5/source/server.py
@@ -21,43 +21,59 @@ class web_server(http.server.SimpleHTTPRequestHandler):
         self.prepare_headers()
         self.wfile.write("Error occurred".encode())
 
-    def do_GET(self):
-
-        print("Request url: " + self.path)
-
-        parsed = urlparse(self.path)
-        params = parse_qs(parsed.query)
-
-        print(params)
-
-        if len(params) != 2:
-            self.send_error_msg()
-            return
-        if 'num1' not in params.keys():
-            self.send_error_msg()
-            return
-        if 'num2' not in params.keys():
-            self.send_error_msg()
-            return
-        try:
-            number_1 = int(params['num1'][0])
-            number_2 = int(params['num2'][0])
-        except ValueError:
-            self.send_error_msg()
-            return
-        if number_2 == 0:
-            self.send_error_msg()
-            return
-        
+    def do_POST(self):
         self.prepare_headers()
-        self.wfile.write(json.dumps(
-            {"sum": number_1 + number_2,
-             "sub": number_1 - number_2,
-             "mul": number_1 * number_2,
-             "div": int(number_1 / number_2),
-             "mod": number_1 % number_2
-             }
-        ).encode())
+        data_len = self.rfile.read(int(self.headers['Content-Length']))
+        input_json = json.loads(data_len)
+        self.wfile.write(json.dumps(input_json).encode())
+        print(input_json)
+        if 'str' in input_json and 'num1' in input_json and 'num2' in input_json:
+            print("1")
+            return
+        if 'num1' in input_json and 'num2' in input_json:
+            print("2")
+            return
+        if 'str' in input_json:
+            print("3")
+            return 
+
+    # def do_GET(self):
+    #
+    #     print("Request url: " + self.path)
+    #
+    #     parsed = urlparse(self.path)
+    #     params = parse_qs(parsed.query)
+    #
+    #     print(params)
+    #
+    #     if len(params) != 2:
+    #         self.send_error_msg()
+    #         return
+    #     if 'num1' not in params.keys():
+    #         self.send_error_msg()
+    #         return
+    #     if 'num2' not in params.keys():
+    #         self.send_error_msg()
+    #         return
+    #     try:
+    #         number_1 = int(params['num1'][0])
+    #         number_2 = int(params['num2'][0])
+    #     except ValueError:
+    #         self.send_error_msg()
+    #         return
+    #     if number_2 == 0:
+    #         self.send_error_msg()
+    #         return
+    #
+    #     self.prepare_headers()
+    #     self.wfile.write(json.dumps(
+    #         {"sum": number_1 + number_2,
+    #          "sub": number_1 - number_2,
+    #          "mul": number_1 * number_2,
+    #          "div": int(number_1 / number_2),
+    #          "mod": number_1 % number_2
+    #          }
+    #     ).encode())
 
 
 # --- main ---
