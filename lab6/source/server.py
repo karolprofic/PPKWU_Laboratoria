0a19ddacbc8ceca78ea3827e8f26b6cebb77952c
Karol Profic <karolprofic@protonmail.com>
2022-12-22 12:53:28 +0100

    Przejście na flask

--- lab6/source/server.py
+++ lab6/source/server.py
@@ -2,8 +2,12 @@
 import http.server
 import json
 import socketserver
+from flask import Flask, request, jsonify, Response
+import xmltodict
+import dicttoxml
+from xml.dom.minidom import parseString
 
-# print('source code for "http.server":', http.server.__file__)
+app = Flask(__name__)
 
 def count_lowercase(string):
     return sum(1 for c in string if c.islower())
@@ -26,80 +30,100 @@ def count_special(string):
     return count
 
 
-class web_server(http.server.SimpleHTTPRequestHandler):
+def send_error_msg(self):
+    self.prepare_headers()
+    self.wfile.write("Error occurred".encode())
 
-    def prepare_headers(self):
-        self.protocol_version = 'HTTP/1.1'
-        self.send_response(200)
-        self.send_header("Content-type", "text/html; charset=UTF-8")
-        self.end_headers()
+def calculator(self, params):
+    if 'num1' not in params.keys():
+        self.send_error_msg()
+        return
+    if 'num2' not in params.keys():
+        self.send_error_msg()
+        return
+    try:
+        number_1 = int(params['num1'])
+        number_2 = int(params['num2'])
+    except ValueError:
+        self.send_error_msg()
+        return
+    if number_2 == 0:
+        self.send_error_msg()
+        return
 
-    def send_error_msg(self):
-        self.prepare_headers()
-        self.wfile.write("Error occurred".encode())
+    return json.dumps({
+        "sum": number_1 + number_2,
+        "sub": number_1 - number_2,
+        "mul": number_1 * number_2,
+        "div": int(number_1 / number_2),
+        "mod": number_1 % number_2
+    })
 
-    def calculator(self, params):
-        if 'num1' not in params.keys():
-            self.send_error_msg()
-            return
-        if 'num2' not in params.keys():
-            self.send_error_msg()
-            return
-        try:
-            number_1 = int(params['num1'])
-            number_2 = int(params['num2'])
-        except ValueError:
-            self.send_error_msg()
-            return
-        if number_2 == 0:
-            self.send_error_msg()
-            return
+@app.post('/')
+def zad6():
+    data_xml = request.get_data()
+    data = xmltodict.parse(data_xml)
 
-        return json.dumps({
-            "sum": number_1 + number_2,
-            "sub": number_1 - number_2,
-            "mul": number_1 * number_2,
-            "div": int(number_1 / number_2),
-            "mod": number_1 % number_2
-        })
+    if 'str' in data:
+        data_to_parse = data['root']
+        if 'str' in data_to_parse:
+            print("str")
 
-    def do_POST(self):
-        self.prepare_headers()
-        data_len = self.rfile.read(int(self.headers['Content-Length']))
-        input_json = json.loads(data_len)
-        self.wfile.write(json.dumps(input_json).encode())
-        print(input_json)
-        if 'str' in input_json and 'num1' in input_json and 'num2' in input_json:
-            print("1")
-            self.prepare_headers()
-            calculate = json.loads(self.calculator(input_json))
-            self.wfile.write(json.dumps({
-                "lowercase": count_lowercase(input_json['str']),
-                "uppercase": count_uppercase(input_json['str']),
-                "digits": count_digit(input_json['str']),
-                "special": count_special(input_json['str']),
-                "sum": calculate['sum'],
-                "sub": calculate['sub'],
-                "mul": calculate['mul'],
-                "div": calculate['div'],
-                "mod": calculate['mod']
-            }).encode())
-            return
-        if 'num1' in input_json and 'num2' in input_json:
-            print("2")
-            self.prepare_headers()
-            self.wfile.write(self.calculator(input_json).encode())
-            return
-        if 'str' in input_json:
-            print("3")
-            self.prepare_headers()
-            self.wfile.write(json.dumps({
-                "lowercase": count_lowercase(input_json['str']),
-                "uppercase": count_uppercase(input_json['str']),
-                "digits": count_digit(input_json['str']),
-                "special": count_special(input_json['str'])
-            }).encode())
-            return
+        if 'num1' in data_to_parse and 'num2' in data_to_parse:
+            print("str i int")
+
+    if 'root' in data:
+        print("int")
+
+
+
+
+    # def do_POST(self):
+    #     print("POST Request")
+    #     self.prepare_headers()
+    #     data_len = self.rfile.read(int(self.headers['Content-Length']))
+    #     x = self.rfile.read()
+    #     # input_json = json.loads(data_len)
+    #     # self.wfile.write(json.dumps(input_json).encode())
+    #     # tree = ET.parse('movies.xml')
+    #     #
+    #
+    #     print(data_len)
+    #     print(x)
+
+
+
+        # if 'str' in input_json and 'num1' in input_json and 'num2' in input_json:
+        #     print("1")
+        #     self.prepare_headers()
+        #     calculate = json.loads(self.calculator(input_json))
+        #     self.wfile.write(json.dumps({
+        #         "lowercase": count_lowercase(input_json['str']),
+        #         "uppercase": count_uppercase(input_json['str']),
+        #         "digits": count_digit(input_json['str']),
+        #         "special": count_special(input_json['str']),
+        #         "sum": calculate['sum'],
+        #         "sub": calculate['sub'],
+        #         "mul": calculate['mul'],
+        #         "div": calculate['div'],
+        #         "mod": calculate['mod']
+        #     }).encode())
+        #     return
+        # if 'num1' in input_json and 'num2' in input_json:
+        #     print("2")
+        #     self.prepare_headers()
+        #     self.wfile.write(self.calculator(input_json).encode())
+        #     return
+        # if 'str' in input_json:
+        #     print("3")
+        #     self.prepare_headers()
+        #     self.wfile.write(json.dumps({
+        #         "lowercase": count_lowercase(input_json['str']),
+        #         "uppercase": count_uppercase(input_json['str']),
+        #         "digits": count_digit(input_json['str']),
+        #         "special": count_special(input_json['str'])
+        #     }).encode())
+        #     return
 
 
 # --- main ---
@@ -108,5 +132,4 @@ PORT = 4080
 
 print(f'Starting: http://localhost:{PORT}')
 
-tcp_server = socketserver.TCPServer(("", PORT), web_server)
-tcp_server.serve_forever()
+app.run(port=4080, host='0.0.0.0')
